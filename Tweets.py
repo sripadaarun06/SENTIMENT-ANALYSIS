64ab633ee589b7093f34e4bd1dec3598ff3446b4
Sripada Arun <150276806+sripadaarun06@users.noreply.github.com>
2025-06-18 17:08:26 +0530

    Update Tweets.py

--- Tweets.py
+++ Tweets.py
@@ -1,10 +1,1 @@
-import pandas as pd
-import numpy as numpy
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import multinomialNB
-from sklearn.metrics import classification_report, confusion_matrix
+
